341953cd55e996b51f55715d07a5c1c237fcb06f
Kenneth Portillo <portillokenneth@gmail.com>
2019-08-14 04:20:20 +0000

    Automatically backed up by Learn

--- lib/introduction.rb
+++ lib/introduction.rb
@@ -11,11 +11,10 @@ def introduction_with_language(name, language)
   puts "Hi, my name is #{name} and I am learning to program in #{language}."
 end
 
-def introduction_with_language_optional(name, language)
+def introduction_with_language_optional(name, language = "Ruby")
   puts "Hi, my name is #{name} and I am learning tp program in #{language}."
 end 
 
-introduction_with_language_optional(name, "javascript")
 
 
 
